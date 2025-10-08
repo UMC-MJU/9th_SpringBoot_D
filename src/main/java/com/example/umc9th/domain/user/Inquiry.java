640741103a56b877a35efc8162abbd8b3829415c
CHAN <150508884+zerochani@users.noreply.github.com>
2025-10-08 20:29:09 +0900

    [FEAT] 사용자 문의 기능을 위한 Inquiry 엔티티 & 문의 답변 기능을 위한 InquiryAnswer 엔티티 추가

---
+++ src/main/java/com/example/umc9th/domain/user/Inquiry.java
@@ -0,0 +1,41 @@
+package com.example.umc9th.domain.user;
+
+import com.example.umc9th.domain.common.BaseTimeEntity;
+import com.example.umc9th.domain.common.Photo;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Inquiry extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @OneToMany(mappedBy = "inquiry", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Photo> photos = new ArrayList<>();
+
+    //문의가 삭제되면 답변도 함께 삭제되도록 설정
+    @OneToOne(mappedBy = "inquiry", cascade = CascadeType.ALL, orphanRemoval = true)
+    private InquiryAnswer inquiryAnswer;
+}
---
+++ src/main/java/com/example/umc9th/domain/user/InquiryAnswer.java
@@ -0,0 +1,26 @@
+package com.example.umc9th.domain.user;
+
+import com.example.umc9th.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InquiryAnswer extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name = "inquiry_id", nullable = false, unique = true)
+    private Inquiry inquiry;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+}

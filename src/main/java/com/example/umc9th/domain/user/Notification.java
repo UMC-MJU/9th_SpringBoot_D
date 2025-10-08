09554e36c60d7ed259dd0a367f066769d239a633
CHAN <150508884+zerochani@users.noreply.github.com>
2025-10-08 20:26:51 +0900

    [FEAT] 사용자의 알림 기능을 위한 Notification 엔티티 추가

---
+++ src/main/java/com/example/umc9th/domain/user/Notification.java
@@ -0,0 +1,40 @@
+package com.example.umc9th.domain.user;
+
+import com.example.umc9th.domain.common.BaseTimeEntity;
+import com.example.umc9th.domain.user.enums.NotificationType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String content;
+
+    //알림 클릭 시 이동할 URL
+    private String relatedUrl;
+
+    @Column(nullable = false)
+    private boolean isRead = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType notificationType;
+
+    //사용자가 알림을 읽음 처리하는 메서드
+    public void markAsRead(){
+        this.isRead = true;
+    }
+}
---
+++ src/main/java/com/example/umc9th/domain/user/enums/NotificationType.java
@@ -0,0 +1,7 @@
+package com.example.umc9th.domain.user.enums;
+
+public enum NotificationType {
+    REVIEW_ANSWERED, //내 리뷰에 답변이 달렸을 때
+    MISSION_SUCCESS, //미션 성공 보상
+    EVENT //기타 이벤트 알림
+}

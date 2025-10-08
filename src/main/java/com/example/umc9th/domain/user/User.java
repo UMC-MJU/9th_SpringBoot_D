3166dc9941bc3b2c9e794015d68344741e91c365
CHAN <150508884+zerochani@users.noreply.github.com>
2025-10-08 20:09:01 +0900

    [FEAT] 사용자 핵심 엔티티 추가

---
+++ src/main/java/com/example/umc9th/domain/user/User.java
@@ -0,0 +1,58 @@
+package com.example.umc9th.domain.user;
+
+import com.example.umc9th.domain.common.BaseTimeEntity;
+import com.example.umc9th.domain.review.Review;
+import com.example.umc9th.domain.user.enums.Gender;
+import com.example.umc9th.domain.user.enums.Role;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<PointTransaction> pointTransactions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Notification> notifications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserFoodPreference> userFoodPreferences = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Inquiry> inquiries = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews = new ArrayList<>();
+}
---
+++ src/main/java/com/example/umc9th/domain/user/enums/Role.java
@@ -0,0 +1,5 @@
+package com.example.umc9th.domain.user.enums;
+
+public enum Role {
+    CUSTOMER, OWNER
+}

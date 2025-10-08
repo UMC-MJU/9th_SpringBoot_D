0a75608be7d04d4c0e2cbebf4f4110873fcfd7f2
CHAN <150508884+zerochani@users.noreply.github.com>
2025-10-08 18:06:05 +0900

    [FEAT] 다형적 관계를 지원하도록 Photo 엔티티 생성

---
+++ src/main/java/com/example/umc9th/domain/common/Photo.java
@@ -0,0 +1,40 @@
+package com.example.umc9th.domain.common;
+
+
+import com.example.umc9th.domain.review.Review;
+import com.example.umc9th.domain.store.Store;
+import com.example.umc9th.domain.user.Inquiry;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Photo extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhotoType photoType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inquiry_id")
+    private Inquiry inquiry;
+}
---
+++ src/main/java/com/example/umc9th/domain/common/PhotoType.java
@@ -0,0 +1,5 @@
+package com.example.umc9th.domain.common;
+
+public enum PhotoType {
+    STORE, REVIEW, INQUIRY
+}

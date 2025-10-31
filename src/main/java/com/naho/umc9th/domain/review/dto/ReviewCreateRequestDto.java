325a7b2ac39b96c116d866fa0e4f6a0e10bddf74
NAHO <ohmap@naver.com>
2025-10-31 17:20:49 +0900

    feat: JPA

--- src/main/java/com/naho/umc9th/domain/review/dto/ReviewCreateRequestDto.java
+++ src/main/java/com/naho/umc9th/domain/review/dto/ReviewCreateRequestDto.java
@@ -4,11 +4,8 @@ package com.naho.umc9th.domain.review.dto;
 
 public record ReviewCreateRequestDto (
         Long memberId,
-
         Long storeId,
-
         Double rating,
-
         String content
 ) {
 

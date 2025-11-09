9eeecc7222b72169b9787200e6b7488f6aab0d01
NAHO <ohmap@naver.com>
2025-11-10 00:34:27 +0900

    feat: ReviewRepositoryCustom 추가

--- src/main/java/com/naho/umc9th/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/naho/umc9th/domain/review/repository/ReviewRepository.java
@@ -3,5 +3,6 @@ package com.naho.umc9th.domain.review.repository;
 import com.naho.umc9th.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+    //Jpa의 기본 메서드 + ReviewRepositoryCustom의 메서드 모두 사용 가능
 }

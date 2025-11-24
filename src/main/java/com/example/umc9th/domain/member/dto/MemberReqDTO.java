b798e501df737d60d3294b053938569299e3b5e8
strfunctionk <comzun01@gmail.com>
2025-11-24 10:15:44 +0900

    feat: dto 메시지 추가

--- src/main/java/com/example/umc9th/domain/member/dto/MemberReqDTO.java
+++ src/main/java/com/example/umc9th/domain/member/dto/MemberReqDTO.java
@@ -4,21 +4,24 @@ import com.example.umc9th.domain.member.enums.Gender;
 import com.example.umc9th.global.annotation.ExistFoods;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
 public class MemberReqDTO {
 
     public record JoinDTO(
-            @NotNull
+            @NotNull(message = "이메일은 필수 입력 값입니다.")
             String email,
-            @NotNull
+            @NotNull(message = "전화번호는 필수 입력 값입니다.")
+            @Pattern(regexp = "01(?:0|1|[6~9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$")
             String phone,
-            @NotBlank
+            @NotBlank(message = "이름은 빈 칸이 될 수 없습니다.")
+            @NotNull(message = "이름은 필수 입력 값입니다.")
             String name,
-            @NotNull
+            @NotNull(message = "성별은 필수 입력 값입니다.")
             Gender gender,
-            @NotNull
+            @NotNull(message = "생년월일은 필수 입력 값입니다.")
             String birth,
             @ExistFoods
             List<Long> preferCategory

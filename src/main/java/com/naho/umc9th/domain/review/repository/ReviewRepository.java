35a2ad512797be4c851d9caa251083dd4d101e46
NAHO <ohmap@naver.com>
2025-11-30 17:49:23 +0900

    feat: findAllByMember 추가

--- src/main/java/com/naho/umc9th/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/naho/umc9th/domain/review/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.naho.umc9th.domain.review.repository;
 
+import com.naho.umc9th.domain.member.entity.Member;
 import com.naho.umc9th.domain.review.entity.Review;
 import com.naho.umc9th.domain.store.entity.Store;
 import org.springframework.data.domain.Page;
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom, ReviewQueryDsl {
     //Jpa의 기본 메서드 + ReviewRepositoryCustom의 메서드 모두 사용 가능
     Page<Review> findAllByStore(Store store, Pageable pageable);
+
+    Page<Review> findAllByMember(Member member, Pageable pageable);
 }

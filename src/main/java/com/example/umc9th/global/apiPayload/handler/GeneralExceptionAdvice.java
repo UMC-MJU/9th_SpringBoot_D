a78ee28aa79fbb99fc33ed1f0d855e815958d83b
strfunctionk <comzun01@gmail.com>
2025-12-01 13:27:50 +0900

    feat: Validated 추가

--- src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -4,6 +4,7 @@ import com.example.umc9th.global.apiPayload.ApiResponse;
 import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
 import com.example.umc9th.global.apiPayload.code.GeneralErrorCode;
 import com.example.umc9th.global.apiPayload.exception.GeneralException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,22 +30,7 @@ public class GeneralExceptionAdvice {
                 );
     }
 
-    // 그 외의 정의되지 않은 모든 예외 처리
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<String>> handleException(
-            Exception ex
-    ) {
-
-        BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
-        return ResponseEntity.status(code.getStatus())
-                .body(ApiResponse.onFailure(
-                                code,
-                                ex.getMessage()
-                        )
-                );
-    }
-
-    // 컨트롤러 메서드에서 @Valid 어노테이션을 사용하여 DTO의 유효성 검사를 수행
+    // 컨트롤러 메서드에서 @Valid 어노테이션을 사용하여 DTO의 유효성 검사를 수행 (400)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException ex
@@ -60,5 +46,42 @@ public class GeneralExceptionAdvice {
 
         // 에러 코드, 메시지와 함께 errors를 반환
         return ResponseEntity.status(code.getStatus()).body(errorResponse);
+    }
+
+    //@RequestParam, @PathVariable의 유효성 검사 실패(ConstraintViolationException) 처리 (400)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleConstraintViolationException(
+            ConstraintViolationException ex
+    ) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getConstraintViolations().forEach(violation -> {
+            String fieldPath = violation.getPropertyPath().toString();
+            String fieldName = fieldPath.substring(fieldPath.lastIndexOf('.') + 1);
+
+            errors.put(fieldName, violation.getMessage());
+        });
+
+        GeneralErrorCode code = GeneralErrorCode.VALID_FAIL; // 400에 해당하는 에러 코드 사용
+        ApiResponse<Map<String, String>> errorResponse = ApiResponse.onFailure(code, errors);
+
+        // 400 상태 코드로 반환
+        return ResponseEntity.status(code.getStatus()).body(errorResponse);
+    }
+
+    // 그 외의 정의되지 않은 모든 예외 처리 (500)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<String>> handleException(
+            Exception ex
+    ) {
+
+        BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(
+                                code,
+                                ex.getMessage()
+                        )
+                );
     }
 }

753639b3a749e4e663f420245de846d5f922d166
NAHO <ohmap@naver.com>
2025-12-13 17:28:49 +0900

    feat: Spring Security

---
+++ src/main/java/com/naho/umc9th/domain/common/auth/enums/Role.java
@@ -0,0 +1,5 @@
+package com.naho.umc9th.domain.common.auth.enums;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/naho/umc9th/domain/common/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.naho.umc9th.domain.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity // Spring Security 설정 활성화, 작성한 코드가 Spring Security 기본 설정보다 우선 적용
+@Configuration
+public class SecurityConfig {
+
+    private final String[] allowUris = {
+            "/sign-up",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "v3/api-docs/**",
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(allowUris).permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/swagger-ui/index.html", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+
+
+}
